106e87e7b5c2172167ed4b0bc55d1981446d18b4
Dbelataris <belataris@outlook.fr>
2018-02-26 17:01:47 +0100

    Géneration d'un username et password aléatoire

--- main.go
+++ main.go
@@ -7,23 +7,24 @@ import (
 )
 
 type Login struct {
-	username string
-	password string
+	Username string
+	Password string
 }
 
-func main() {
-	for i := 0; i < 10; i++ {
-		fmt.Printf("%v\n", generateLogin(8))
-	}
-}
-
-func generateLogin(nbreCara int) string {
+func (log *Login) generateLogin(nbreCara int) {
 	const base string = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 	b := make([]byte, nbreCara)
 	for i := range b {
 		rand.Seed(time.Now().UnixNano())
 		b[i] = base[rand.Intn(len(base))]
 	}
-	return string(b)
-
+	log.Username = string(b)
+	log.Password = string(b)
+}
+func main() {
+	for i := 0; i < 10; i++ {
+		var login1 Login
+		login1.generateLogin(8)
+		fmt.Printf("%v", login1)
+	}
 }
