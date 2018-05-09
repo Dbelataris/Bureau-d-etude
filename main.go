a9e2621bd059daf4aa2ed06f11bde88233fb16fc
Dbelataris <belataris@outlook.fr>
2018-05-09 15:18:29 +0200

    [ADDS] Ajout fichier de config + suppression user

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"bufio"
 	"database/sql"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"strconv"
+	"strings"
 	"time"
 
 	. "Bureau-d-etude/tools"
@@ -13,6 +18,9 @@ import (
 
 var Db *sql.DB
 var Ok bool
+var TEx int   //Temps Expiration
+var TUtil int //Temps utilisation
+var StrTimes []string
 
 func main() {
 	/*var LoginsValides = []Login{}
@@ -25,7 +33,14 @@ func main() {
 	}
 	defer Db.Close()
 	Ok = true
+	readLine("./parameters")
+	fmt.Println("Tex:" + StrTimes[0] + "	Tutil:" + StrTimes[1])
+	TEx, _ = strconv.Atoi(StrTimes[0])
+
+	TUtil, _ = strconv.Atoi(StrTimes[1])
+
 	http.HandleFunc("/getLogin", LoginHandleFunc)
+	http.HandleFunc("/delLogin", DeleteHandleFunc)
 	http.ListenAndServe(":8080", nil)
 
 }
@@ -34,7 +49,7 @@ func LoginHandleFunc(w http.ResponseWriter, r *http.Request) {
 	var loginGen1 Login
 
 	if Ok == true {
-		loginGen1.GenerateLogin(8, 2, 5)
+		loginGen1.GenerateLogin(8, TEx, TUtil)
 		StoreLogin(Db, loginGen1)
 		fmt.Println(loginGen1)
 		w.Header().Add("Content-Type", "application/json; charset=utf-8")
@@ -50,4 +65,32 @@ func LoginHandleFunc(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("Stop")
 	}*/
 
+}
+
+func DeleteHandleFunc(w http.ResponseWriter, r *http.Request) {
+	param, ok := r.URL.Query()["username"]
+
+	if !ok || len(param) < 1 {
+		log.Println("Url Param 'username' est manquant")
+		return
+	} else {
+		DeleteLogin(Db, param[0])
+	}
+
+}
+
+func readLine(path string) {
+	var line []string
+	inFile, _ := os.Open(path)
+	defer inFile.Close()
+	scanner := bufio.NewScanner(inFile)
+	scanner.Split(bufio.ScanLines)
+
+	for scanner.Scan() {
+		//fmt.Println(scanner.Text() + "***")
+		line = strings.Split(scanner.Text(), " ")
+		/*fmt.Println(Line[2])*/
+		StrTimes = append(StrTimes, line[2])
+
+	}
 }
